62cfd4c49363ec244b81ee3cb173e7bb058cd893
Malachitt <ilia.naymenko@gmail.com>
2018-12-05 21:13:42 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp6/Program1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+	public partial class Person
+	{
+		partial void DoSomethingElse();
+
+		public void DoSomething()
+		{
+			Console.WriteLine("Начало первого класса");
+			DoSomethingElse();
+			Console.WriteLine("Конец первого класса");
+		}
+	}
+}
---
+++ ConsoleApp6/Program2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+	public partial class Person
+	{
+		partial void DoSomethingElse()
+		{
+			Console.WriteLine("Содержимое второго класса");
+		}
+	}
+}
